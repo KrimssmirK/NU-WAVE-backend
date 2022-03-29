c963186682e4eb741262c262b4eacae778c3ef16
KrimssmirK <72426749+KrimssmirK@users.noreply.github.com>
2022-03-29 10:28:02 +0900

    removed redundancy

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 // libraries
 const express = require('express')
 const mongoose = require('mongoose')
-// const path = require('path')
 
 // routes
 const exhibitRoutes = require('./routes/exhibit')
 const productRoutes = require('./routes/product')
-// const galleryRoutes = require('./routes/gallery')
 
 const app = express()
 app.set("view engine", "ejs")
@@ -17,21 +15,15 @@ app.use(function(req, res, next) {
   next();
   });
 
-// const isReactServer = false
-// if (!isReactServer) {
-//   app.use('/', express.static(path.join(__dirname, 'public')))
-// }
-
 const guestController = require('./controllers/guestController')
 app.use('/', guestController.guest_visit_the_website)
 
 // API
 app.use('/api', exhibitRoutes)
 app.use('/api', productRoutes)
-// app.use('/api', galleryRoutes)
 
 // const local_DB_URI = 'mongodb://127.0.0.1:27017/centie'
-// const local_DB_ATLAS = 'mongodb+srv://krimssmirk:t1uYuNFqKRlXhWmr@cluster0.21jlv.mongodb.net/centieDev?retryWrites=true&w=majority'
+const local_DB_ATLAS = 'mongodb+srv://krimssmirk:t1uYuNFqKRlXhWmr@cluster0.21jlv.mongodb.net/centie?retryWrites=true&w=majority'
 const dbURI = process.env.MONGODB_URI || local_DB_ATLAS || local_DB_URI
 const port = process.env.PORT || 3000
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
